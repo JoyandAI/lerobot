352752085a6a182733a1937ce8ca5f1baeff8aed
Axel <1470582559@qq.com>
2026-05-29 18:21:26 +0800

    add HLS motor support

--- src/lerobot/motors/feetech/tables.py
+++ src/lerobot/motors/feetech/tables.py
@@ -151,6 +151,78 @@ SCS_SERIES_CONTROL_TABLE = {
     "Acceleration_2": (83, 1),  # don't know what that is
 }
 
+# http://doc.feetech.cn/#/f?q=250689e628d7
+HLS_SERIES_CONTROL_TABLE = {
+    # Version information
+    "Firmware_Major_Version": FIRMWARE_MAJOR_VERSION,  # read-only
+    "Firmware_Minor_Version": FIRMWARE_MINOR_VERSION,  # read-only
+    "Model_Number": MODEL_NUMBER,  # read-only
+    # EPROM
+    "ID": (5, 1),
+    "Baud_Rate": (6, 1),
+    "Secondary_ID": (7, 1),
+    "Response_Status_Level": (8, 1),
+    "Min_Position_Limit": (9, 2),
+    "Max_Position_Limit": (11, 2),
+    "Max_Temperature_Limit": (13, 1),
+    "Max_Voltage_Limit": (14, 1),
+    "Min_Voltage_Limit": (15, 1),
+    "Max_Torque_Limit": (16, 2),
+    "Phase": (18, 1),
+    "Unloading_Condition": (19, 1),
+    "LED_Alarm_Condition": (20, 1),
+    "P_Coefficient": (21, 1),
+    "D_Coefficient": (22, 1),
+    "I_Coefficient": (23, 1),
+    "Minimum_Startup_Force": (24, 1),
+    "Integral_Limit": (25, 1),
+    "CW_Dead_Zone": (26, 1),
+    "CCW_Dead_Zone": (27, 1),
+    "Protection_Current": (28, 2),
+    "Angular_Resolution": (30, 1),
+    "Homing_Offset": (31, 2),
+    "Operating_Mode": (33, 1),
+    "Current_Loop_P_Coefficient": (34, 1),
+    "Current_Loop_I_Coefficient": (35, 1),
+    "Velocity_closed_loop_P_proportional_coefficient": (37, 1),
+    "Over_Current_Protection_Time": (38, 1),
+    "Velocity_closed_loop_I_integral_coefficient": (39, 1),
+    # SRAM control
+    "Torque_Enable": (40, 1),
+    "Acceleration": (41, 1),
+    "Goal_Position": (42, 2),
+    "Goal_Current": (44, 2),
+    "Goal_Velocity": (46, 2),
+    "Torque_Limit": (48, 2),
+    "Kp": (50, 1),
+    "Kd": (51, 1),
+    "Ki": (52, 1),
+    "Lock": (55, 1),
+    # SRAM feedback
+    "Present_Position": (56, 2),  # read-only
+    "Present_Velocity": (58, 2),  # read-only
+    "Present_Load": (60, 2),  # read-only
+    "Present_Voltage": (62, 1),  # read-only
+    "Present_Temperature": (63, 1),  # read-only
+    "Async_Write_Flag": (64, 1),  # read-only
+    "Status": (65, 1),  # read-only
+    "Moving": (66, 1),  # read-only
+    "Goal_Position_2": (67, 2),  # read-only
+    "Present_Current": (69, 2),  # read-only
+    "Current_Offset": (73, 2),  # read-only
+    # Factory
+    "vFk*10": (77, 1),
+    "vKgI": (78, 1),
+    "pFk*10": (79, 1),
+    "Moving_Velocity_Threshold": (80, 1),
+    "DTs": (81, 1),  # (ms)
+    "eFk*10": (82, 1),
+    "Vk": (83, 1),  # (ms)
+    "Maximum_Velocity_Limit": (84, 1),
+    "Maximum_Acceleration": (85, 1),
+    "Acceleration_Multiplier": (86, 1),
+}
+
 STS_SMS_SERIES_BAUDRATE_TABLE = {
     1_000_000: 0,
     500_000: 1,
@@ -176,31 +248,37 @@ SCS_SERIES_BAUDRATE_TABLE = {
 MODEL_CONTROL_TABLE = {
     "sts_series": STS_SMS_SERIES_CONTROL_TABLE,
     "scs_series": SCS_SERIES_CONTROL_TABLE,
+    "hls_series": HLS_SERIES_CONTROL_TABLE,
     "sms_series": STS_SMS_SERIES_CONTROL_TABLE,
     "sts3215": STS_SMS_SERIES_CONTROL_TABLE,
     "sts3250": STS_SMS_SERIES_CONTROL_TABLE,
     "scs0009": SCS_SERIES_CONTROL_TABLE,
     "sm8512bl": STS_SMS_SERIES_CONTROL_TABLE,
+    "hls3915m": HLS_SERIES_CONTROL_TABLE,
 }
 
 MODEL_RESOLUTION = {
     "sts_series": 4096,
     "sms_series": 4096,
     "scs_series": 1024,
+    "hls_series": 4096,
     "sts3215": 4096,
     "sts3250": 4096,
     "sm8512bl": 4096,
     "scs0009": 1024,
+    "hls3915m": 4096,
 }
 
 MODEL_BAUDRATE_TABLE = {
     "sts_series": STS_SMS_SERIES_BAUDRATE_TABLE,
     "sms_series": STS_SMS_SERIES_BAUDRATE_TABLE,
     "scs_series": SCS_SERIES_BAUDRATE_TABLE,
+    "hls_series": STS_SMS_SERIES_BAUDRATE_TABLE,
     "sm8512bl": STS_SMS_SERIES_BAUDRATE_TABLE,
     "sts3215": STS_SMS_SERIES_BAUDRATE_TABLE,
     "sts3250": STS_SMS_SERIES_BAUDRATE_TABLE,
     "scs0009": SCS_SERIES_BAUDRATE_TABLE,
+    "hls3915m": STS_SMS_SERIES_BAUDRATE_TABLE,
 }
 
 # Sign-Magnitude encoding bits
@@ -219,10 +297,12 @@ MODEL_ENCODING_TABLE = {
     "sts_series": STS_SMS_SERIES_ENCODINGS_TABLE,
     "sms_series": STS_SMS_SERIES_ENCODINGS_TABLE,
     "scs_series": {},
+    "hls_series": STS_SMS_SERIES_ENCODINGS_TABLE,
     "sts3215": STS_SMS_SERIES_ENCODINGS_TABLE,
     "sts3250": STS_SMS_SERIES_ENCODINGS_TABLE,
     "sm8512bl": STS_SMS_SERIES_ENCODINGS_TABLE,
     "scs0009": {},
+    "hls3915m": STS_SMS_SERIES_ENCODINGS_TABLE,
 }
 
 SCAN_BAUDRATES = [
@@ -244,14 +324,17 @@ MODEL_NUMBER_TABLE = {
     "sts3250": 2825,
     "sm8512bl": 11272,
     "scs0009": 1284,
+    "hls3915m": 5130,
 }
 
 MODEL_PROTOCOL = {
     "sts_series": 0,
     "sms_series": 0,
     "scs_series": 1,
+    "hls_series": 0,
     "sts3215": 0,
     "sts3250": 0,
     "sm8512bl": 0,
     "scs0009": 1,
+    "hls3915m": 0,
 }
