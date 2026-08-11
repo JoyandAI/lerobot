f0fabb84f2cdd1d0b1415d30f260effcef7a0584
Axel <1470582559@qq.com>
2026-08-11 11:51:59 +0800

    sync auto_calibrate_example servo params with auto_calibrate_ui

--- examples/calibrate/auto_calibrate_example.py
+++ examples/calibrate/auto_calibrate_example.py
@@ -38,19 +38,19 @@ DEVICE_FACTORIES = {
 
 AUTO_CALIBRATION_DEFAULTS = {
     "tele": {
-        "try_torque": 400,
-        "max_torque": 500,
+        "try_torque": 600,
+        "max_torque": 600,
         "torque_step": 50,
-        "explore_velocity": 600,
+        "explore_velocity": 400,
         "wait_time_s": 0.5,
         "velocity_threshold": 4,
         "position_tolerance": 4000,
     },
     "robot": {
-        "try_torque": 600,
-        "max_torque": 1000,
+        "try_torque": 500,
+        "max_torque": 600,
         "torque_step": 50,
-        "explore_velocity": 800,
+        "explore_velocity": 600,
         "wait_time_s": 0.5,
         "velocity_threshold": 4,
         "position_tolerance": 4000,
